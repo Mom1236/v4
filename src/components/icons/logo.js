ba49b05d1629ff9abf0202d398063423df38da31
Mom1236 <89107436+Mom1236@users.noreply.github.com>
2024-07-08 00:33:38 -0500

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,25 +1,3 @@
-import React from 'react';
-
-const IconLogo = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
-    <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
-        <polygon
-          id="Shape"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-        />
-        <path
-          d="M30 30 L30 66 L44 66 C49 66 53 62 53 57 L53 39 C53 34 49 30 44 30 Z M44 34 C47 34 50 37 50 39 L50 45 C50 47 47 50 44 50 L34 50 L34 34 Z M34 50 L34 66"
-          fill="currentColor"
-        />
-      </g>
-    </g>
-  </svg>
-);
+import React from "react";const IconLogo = ({size=24, color="#000000"}) => (<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 19V6M5 12l7-7 7 7"/></svg>);export default ArrowUp;
 
 export default IconLogo;
