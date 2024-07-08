cc57224b38798bfe4fbb132c8ead2a1b8ad390d1
Mom1236 <89107436+Mom1236@users.noreply.github.com>
2024-07-08 13:15:20 -0500

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,15 +1,6 @@
 import React from 'react';
 
 const IconLogo = () => (
- <g id="SVGRepo_bgCarrier" stroke-width="0">
-
-<rect x="-51.2" y="-51.2" width="614.40" height="614.40" rx="0" fill="#050505" strokewidth="0"/>
-
-</g>
-
-<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
-
-<g id="SVGRepo_iconCarrier"> <g id="7935ec95c421cee6d86eb22ecd128885"> <path style="display: inline; fffill-rule: evenodd; clip-rule: evenodd;" d="M368.034,0.5H116.776L5.444,511.5h147.454l36.104-169.294 h105.301c100.803,0,185.075-62.128,207.644-167.747C527.509,54.869,441.751,0.5,368.034,0.5z M363.518,174.459 c-9.032,38.824-46.635,69.888-84.254,69.888h-69.203l31.595-139.776h72.208C349.98,104.571,374.047,135.636,363.518,174.459z"> </path> </g> </g>
-);
+ <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.15" d="M4 6C4 5.44771 4.44772 5 5 5H19C19.5523 5 20 5.44772 20 6V15C20 15.5523 19.5523 16 19 16H5C4.44772 16 4 15.5523 4 15V6Z" fill="#001A72"></path> <path d="M12 16V20M12 20H16M12 20H8M5 16H19C19.5523 16 20 15.5523 20 15V6C20 5.44772 19.5523 5 19 5H5C4.44772 5 4 5.44771 4 6V15C4 15.5523 4.44772 16 5 16Z" stroke="#001A72" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>);
 
 export default IconLogo;
