7408bc32ac9c6df6f0c44c5c91d251713f7392cd
Mom1236 <89107436+Mom1236@users.noreply.github.com>
2024-07-08 13:01:57 -0500

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="3" height="3" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"<line x1="12" y1="1" x2="12" y2="23">></line><path d="M368.034,0.5H116.776L5.444,511.5h147.454l36.104-169.294 h105.301c100.803,0,185.075-62.128,207.644-167.747C527.509,54.869,441.751,0.5,368.034,0.5z M363.518,174.459 c-9.032,38.824-46.635,69.888-84.254,69.888h-69.203l31.595-139.776h72.208C349.98,104.571,374.047,135.636,363.518,174.459z"></path></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="3" height="3" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M368.034,0.5H116.776L5.444,511.5h147.454l36.104-169.294 h105.301c100.803,0,185.075-62.128,207.644-167.747C527.509,54.869,441.751,0.5,368.034,0.5z M363.518,174.459 c-9.032,38.824-46.635,69.888-84.254,69.888h-69.203l31.595-139.776h72.208C349.98,104.571,374.047,135.636,363.518,174.459z"></path></svg>
 );
 
 export default IconLogo;
