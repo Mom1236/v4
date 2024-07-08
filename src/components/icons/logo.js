daacb0f8e59e04e058fbb1e954a48eb1f347d90f
Mom1236 <89107436+Mom1236@users.noreply.github.com>
2024-07-08 00:43:25 -0500

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M 306.63 363.69 
+L 306.63 155.79 
+C 306.63 72.63 389.79 72.63 389.79 155.79 
+L 389.79 197.37 
+C 389.79 238.95 348.21 238.95 306.63 238.95 
+L 306.63 363.69 
+z"></path></svg>
 );
 
 export default IconLogo;
