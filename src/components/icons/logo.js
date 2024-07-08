142ad6368bed65a0425f1d47d77c637a1b186455
Mom1236 <89107436+Mom1236@users.noreply.github.com>
2024-07-08 12:39:37 -0500

    Update logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M 306.63 363.69 
-L 306.63 155.79 
-C 306.63 72.63 389.79 72.63 389.79 155.79 
-L 389.79 197.37 
-C 389.79 238.95 348.21 238.95 306.63 238.95 
-L 306.63 363.69 
-z"></path></svg>
+ <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.15" d="M4 6C4 5.44771 4.44772 5 5 5H19C19.5523 5 20 5.44772 20 6V15C20 15.5523 19.5523 16 19 16H5C4.44772 16 4 15.5523 4 15V6Z" fill="#001A72"></path> <path d="M12 16V20M12 20H16M12 20H8M5 16H19C19.5523 16 20 15.5523 20 15V6C20 5.44772 19.5523 5 19 5H5C4.44772 5 4 5.44771 4 6V15C4 15.5523 4.44772 16 5 16Z" stroke="#001A72" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
 );
 
 export default IconLogo;
