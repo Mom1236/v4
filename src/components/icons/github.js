1d5dc345e2b21b37a04a9a20aaa958ce1414ede9
Mom1236 <89107436+Mom1236@users.noreply.github.com>
2024-07-09 21:00:23 -0500

    Update github.js

--- src/components/icons/github.js
+++ src/components/icons/github.js
@@ -4,7 +4,7 @@ const IconGitHub = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     role="img"
-    viewBox="0 0 24 24"
+    viewBox="0 0 70 70"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
